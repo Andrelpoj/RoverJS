b6489b4362b1086a52a041c3df75c92b7e6837a6
André Pereira <andre.pereira@bemobi.com.br>
2023-03-02 10:01:22 -0300

    Update process to use Typescript

--- index.js
+++ index.js
@@ -1,65 +1,9 @@
 import { argv } from "node:process"
 import readline from "node:readline"
 import fs from "node:fs"
-
 import path from "node:path"
 
-const move = (plateau, position) => {
-	const [limitX, limitY] = plateau
-	const [x, y, direction] = position
-
-	let newX, newY
-	switch (direction) {
-		case "N":
-			newY = y + 1 <= limitY ? y + 1 : limitY
-			return [x, newY]
-		case "S":
-			newY = y - 1 >= 0 ? y - 1 : 0
-			return [x, newY]
-		case "W":
-			newX = x - 1 >= 0 ? x - 1 : 0
-			return [newX, y]
-		case "E":
-			newX = x + 1 <= limitX ? x + 1 : limitX
-			return [newX, y]
-	}
-}
-
-const rotate = (current, rotate) => {
-	switch (current) {
-		case "N":
-			if (rotate === "L") return "W"
-			return "E"
-		case "S":
-			if (rotate === "L") return "E"
-			return "W"
-		case "W":
-			if (rotate === "L") return "S"
-			return "N"
-		case "E":
-			if (rotate === "L") return "N"
-			return "S"
-	}
-}
-
-const process = (plateau, position, instructions) => {
-	let [x, y, direction] = position
-
-	instructions.split("").forEach(instruction => {
-		if (instruction === "M") {
-			const result = move(plateau, [x, y, direction])
-			x = result[0]
-			y = result[1]
-		}
-
-		if (["L", "R"].indexOf(instruction) !== -1) {
-			direction = rotate(direction, instruction)
-		}
-		//TODO: Other cases should throw error
-	})
-
-	console.log(`${x} ${y} ${direction}`)
-}
+import { execute } from "./process.js"
 
 const readInterface = readline.createInterface({
 	input: fs.createReadStream(path.join("./", argv[2])),
@@ -87,5 +31,5 @@ readInterface.on("line", function (line) {
 	}
 
 	instructions = line
-	process(plateau, position, instructions)
+	execute(plateau, position, instructions)
 })
---
+++ process.ts
@@ -0,0 +1,98 @@
+type Direction = "N" | "S" | "W" | "E"
+
+type Rotation = "L" | "R"
+
+type Move = "M"
+
+type Position = {
+	x: number
+	y: number
+	direction: Direction
+}
+
+type Plateau = {
+	x: number
+	y: number
+}
+
+type Instruction = Move | Rotation
+
+const move = (plateau: Plateau, position: Position): Position => {
+	let newX: number, newY: number
+	switch (position.direction) {
+		case "N":
+			newY = position.y + 1 <= plateau.y ? position.y + 1 : plateau.y
+			return {
+				x: position.x,
+				y: newY,
+				direction: position.direction,
+			}
+		case "S":
+			newY = position.y - 1 >= 0 ? position.y - 1 : 0
+			return {
+				x: position.x,
+				y: newY,
+				direction: position.direction,
+			}
+		case "W":
+			newX = position.x - 1 >= 0 ? position.x - 1 : 0
+			return {
+				x: newX,
+				y: position.y,
+				direction: position.direction,
+			}
+		case "E":
+			newX = position.x + 1 <= plateau.x ? position.x + 1 : plateau.x
+			return {
+				x: newX,
+				y: position.y,
+				direction: position.direction,
+			}
+		default:
+			//TODO: Throw error
+			return position
+	}
+}
+
+const rotate = (current: Direction, rotate: Rotation) => {
+	switch (current) {
+		case "N":
+			if (rotate === "L") return "W"
+			return "E"
+		case "S":
+			if (rotate === "L") return "E"
+			return "W"
+		case "W":
+			if (rotate === "L") return "S"
+			return "N"
+		case "E":
+			if (rotate === "L") return "N"
+			return "S"
+	}
+}
+
+const execute = (
+	plateau: Plateau,
+	position: Position,
+	instructions: Instruction[]
+) => {
+	instructions.forEach(instruction => {
+		if (instruction === "M") {
+			const result = move(plateau, position)
+			position.x = result.x
+			position.y = result.y
+		}
+
+		if (["L", "R"].indexOf(instruction) !== -1) {
+			position.direction = rotate(
+				position.direction,
+				instruction as Rotation
+			)
+		}
+		//TODO: Other cases should throw error
+	})
+
+	console.log(`${position.x} ${position.y} ${position.direction}`)
+}
+
+export { execute }
