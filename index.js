c81afb9d844a6d3b7aa1e53343807a60881e3930
André Pereira <andre.pereira@bemobi.com.br>
2023-03-01 22:35:44 -0300

    Add read from file logic

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import { argv } from "node:process"
+import readline from "node:readline"
+import fs from "node:fs"
 
-let x = +argv[2]
-let y = +argv[3]
-let direction = argv[4]
-const instructions = Array(argv[5])
+import path from "node:path"
 
-//TODO: Consider plateau size as finite
+const move = (plateau, position) => {
+	const [limitX, limitY] = plateau
+	const [x, y, direction] = position
 
-const move = (x, y, direction) => {
+	let newX, newY
 	switch (direction) {
 		case "N":
-			return [x, y + 1]
+			newY = y + 1 <= limitY ? y + 1 : limitY
+			return [x, newY]
 		case "S":
-			return [x, y - 1]
+			newY = y - 1 >= 0 ? y - 1 : 0
+			return [x, newY]
 		case "W":
-			return [x - 1, y]
+			newX = x - 1 >= 0 ? x - 1 : 0
+			return [newX, y]
 		case "E":
-			return [x + 1, y]
+			newX = x + 1 <= limitX ? x + 1 : limitX
+			return [newX, y]
 	}
 }
 
@@ -37,18 +42,50 @@ const rotate = (current, rotate) => {
 	}
 }
 
-instructions.forEach(instruction => {
-	if (instruction === "M") {
-		const result = move(x, y, direction)
-		x = result[0]
-		y = result[1]
-	}
+const process = (plateau, position, instructions) => {
+	let [x, y, direction] = position
 
-	if (["L", "R"].indexOf(instruction) !== -1) {
-		direction = rotate(direction, instruction)
-	}
+	instructions.split("").forEach(instruction => {
+		if (instruction === "M") {
+			const result = move(plateau, [x, y, direction])
+			x = result[0]
+			y = result[1]
+		}
 
-	//TODO: Other cases should throw error
+		if (["L", "R"].indexOf(instruction) !== -1) {
+			direction = rotate(direction, instruction)
+		}
+		//TODO: Other cases should throw error
+	})
+
+	console.log(`${x} ${y} ${direction}`)
+}
+
+const readInterface = readline.createInterface({
+	input: fs.createReadStream(path.join("./", argv[2])),
 })
 
-console.log(`${x} ${y} ${direction}`)
+let currentLine = -1
+let plateau = ""
+let position = ""
+let instructions = ""
+readInterface.on("line", function (line) {
+	++currentLine
+
+	//TODO: Add validations for plateau, position and instructions lines
+	if (currentLine === 0) {
+		const raw = line.split(" ")
+		plateau = [+raw[0], +raw[1]]
+		return
+	}
+
+	const isPosition = currentLine % 2 === 1
+	if (isPosition) {
+		const raw = line.split(" ")
+		position = [+raw[0], +raw[1], raw[2]]
+		return
+	}
+
+	instructions = line
+	process(plateau, position, instructions)
+})
